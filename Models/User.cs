f0444a682efb489e6ccd0a1a43f0264ba85a3d16
1337benny <168416093+1337benny@users.noreply.github.com>
2024-12-20 11:26:07 +0100

    Skapande av User klassen

---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace Grupp14_CV.Models
+{
+    public class User : IdentityUser
+    {
+        public User()
+        { 
+            
+        }
+        [Required]
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public bool PublicSetting { get; set; }
+
+
+        // public CV cvid foreign key
+
+    }
+}
