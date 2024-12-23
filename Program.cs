c21627e29dca981e2500e297a9e813e46cb86f44
1337benny <168416093+1337benny@users.noreply.github.com>
2024-12-23 11:23:55 +0100

    Update Program.cs
    builder för context i program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
+using Grupp14_CV.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<UserContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("UserContext")));
+
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
