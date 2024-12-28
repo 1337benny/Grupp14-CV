e40c73addf06e458d526429a33aa4969465fbe48
1337benny <168416093+1337benny@users.noreply.github.com>
2024-12-28 12:21:20 +0100

    Registrerings sida

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -21,12 +21,76 @@ namespace Grupp14_CV.Controllers
             return View();
         }
 
+        //[HttpPost]
+        //public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        User anvandare = new User();
+        //        anvandare.Email = registerViewModel.Epost;
+        //        anvandare.Firstname = registerViewModel.Fornamn;
+        //        anvandare.Lastname = registerViewModel.Efternamn;
+        //        anvandare.BirthDay = registerViewModel.FodelseDatum;
+
+        //        var result =
+        //        await userManager.CreateAsync(anvandare, registerViewModel.Losenord);
+        //        if (result.Succeeded)
+        //        {
+        //            await signInManager.SignInAsync(anvandare, isPersistent: true);
+        //            return RedirectToAction("Index", "Home");
+        //        }
+        //        else
+        //        {
+        //            foreach (var error in result.Errors)
+        //            {
+        //                ModelState.AddModelError("", error.Description);
+        //            }
+        //        }
+        //    }
+        //    return View(registerViewModel);
+        //}
+
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel registerViewModel)
         {
-            //Massor med kod och kontroll. Finns exempel i birdreport
+            if (ModelState.IsValid)
+            {
+                User anvandare = new User
+                {
+                    Email = registerViewModel.Epost,
+                    Firstname = registerViewModel.Fornamn,
+                    Lastname = registerViewModel.Efternamn,
+                    BirthDay = registerViewModel.FodelseDatum
+                };
+
+                var result = await userManager.CreateAsync(anvandare, registerViewModel.Losenord);
+                if (result.Succeeded)
+                {
+                    await signInManager.SignInAsync(anvandare, isPersistent: true);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        if (error.Code == "DuplicateUserName")
+                        {
+                            ModelState.AddModelError("Epost", "E-posten är redan registrerad.");
+                        }
+                        else if (error.Code == "PasswordTooShort")
+                        {
+                            ModelState.AddModelError("Losenord", "Lösenordet är för kort.");
+                        }
+                        else
+                        {
+                            ModelState.AddModelError("", error.Description);
+                        }
+                    }
+                }
+            }
             return View(registerViewModel);
         }
+
 
         [HttpGet]
         public IActionResult LogIn()
--- Models/RegisterViewModel.cs
+++ Models/RegisterViewModel.cs
@@ -4,16 +4,31 @@ namespace Grupp14_CV.Models
 {
     public class RegisterViewModel
     {
-        [Required(ErrorMessage = "Vänligen skriv ett användarnamn.")]
+        [Required(ErrorMessage = "Vänligen skriv en epost.")]
         [StringLength(255)]
-        public string AnvandarNamn { get; set; }
+        public string Epost { get; set; }
+
         [Required(ErrorMessage = "Vänligen skriv lösenord.")]
         [DataType(DataType.Password)]
         [Compare("BekraftaLosenord")]
         public string Losenord { get; set; }
+
+
         [Required(ErrorMessage = "Vänlingen bekräfta lösenordet")]
         [DataType(DataType.Password)]
         [Display(Name = "Bekrafta losenordet")]
         public string BekraftaLosenord { get; set; }
+
+        [Required(ErrorMessage = "Vänligen fyll i ditt förnamn.")]
+        [StringLength(50)]
+        public string Fornamn { get; set; }
+
+        [Required(ErrorMessage = "Vänligen fyll i ditt efternamn.")]
+        [StringLength(50)]
+        public string Efternamn { get; set; }
+
+        [Required(ErrorMessage = "Vänligen fyll i ditt födelsedatum.")]
+        public DateOnly FodelseDatum { get; set; }
+
     }
 }
