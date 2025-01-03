0b3ec42f8a1e425a0255220edc4edaff6dca6861
1337benny <168416093+1337benny@users.noreply.github.com>
2025-01-03 11:20:03 +0100

    ProjektInfo och Gå med i projekt

--- Controllers/ProjectController.cs
+++ Controllers/ProjectController.cs
@@ -13,7 +13,7 @@ namespace Grupp14_CV.Controllers
             projects = service;
         }
 
-        
+
         public IActionResult Project()
         {
             IQueryable<Project> projectList = from project in projects.Projects select project;
@@ -24,7 +24,7 @@ namespace Grupp14_CV.Controllers
         {
             Project project = new Project();
             List<SelectListItem> users = projects.Users.Select
-                (x => new SelectListItem { Text = x.Firstname, Value = x.Id.ToString()}).ToList();
+                (x => new SelectListItem { Text = x.Firstname, Value = x.Id.ToString() }).ToList();
             ViewBag.options = users;
             return View(project);
         }
@@ -72,22 +72,44 @@ namespace Grupp14_CV.Controllers
             }
         }
 
-        //[HttpPost]
-        //public IActionResult AddProject(Project project)
-        //{
-        //    if(ModelState.IsValid)
-        //    {
-        //        project.CreatorID = projects.Users.FirstOrDefault(x => x.UserName == User.Identity.Name).Id;
-        //        projects.Add(project);
-        //        projects.SaveChanges();
-        //        return RedirectToAction("Project", "Project");
-        //    } else
-        //    {
-        //        List<SelectListItem> users = projects.Users.Select
-        //         (x => new SelectListItem { Text = x.Firstname, Value = x.Id.ToString() }).ToList();
-        //        ViewBag.options = users;
-        //        return View(project);
-        //    }
-        //}
+        [HttpGet]
+        public IActionResult ProjectInfo(int projectID)
+        {
+            IQueryable<Project> projectList = from project in projects.Projects where project.ProjectID == projectID select project;
+
+            return View(projectList.FirstOrDefault());
+        }
+
+        [HttpPost]
+        public IActionResult JoinProject(int projectID)
+        {
+            if (ModelState.IsValid)
+            {
+                // Hämta den inloggade användarens ID
+                var username = User.Identity.Name;
+                var user = projects.Users.FirstOrDefault(x => x.UserName == username);
+
+
+                //Lägger till användaren och projektet i sambandstabellen.
+                Users_In_Project user_project = new Users_In_Project();
+                user_project.UserID = user.Id;
+                user_project.ProjectID = projectID; //Sätter projektID till projektet som precis skapats.
+                projects.Add(user_project);
+
+                //Sparar sambandet i databasen
+                projects.SaveChanges();
+
+                return RedirectToAction("Project", "Project");
+            }
+            else
+            {
+                // Förbered användarvalen för ViewBag vid valideringsfel
+                List<SelectListItem> users = projects.Users.Select
+                    (x => new SelectListItem { Text = x.Firstname, Value = x.Id.ToString() }).ToList();
+                ViewBag.options = users;
+
+                return RedirectToAction("Project", "Project");
+            }
+        }
     }
 }
